45808b50f85bfdf4b33355f1b8af01209000acec
AYehia0 <ahmedyehia611@gmail.com>
2021-10-19 00:26:28 +0200

    tuned some comments

--- src/popup.js
+++ src/popup.js
@@ -1,47 +1,37 @@
-// guide: https://developer.chrome.com/docs/extensions/reference/tabs/#method-query
-
-/*
-EXAMPLE:
-https://medium.com/volosoft/whats-new-in-rxjs-7-a11cc564c6c0 -> https://volosoft.medium.com/whats-new-in-rxjs-7-a11cc564c6c0
-*/
+// GUIDE : https://developer.chrome.com/docs/extensions/mv3/getstarted/
+// method-query : https://developer.chrome.com/docs/extensions/reference/tabs/#method-query
 
 document.addEventListener("DOMContentLoaded", async () => {
-  const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
-  const currentTab = { data: tabs[0], url: tabs[0].url };
+  const tabs = await chrome.tabs.query({ active: true, currentWindow: true })
+  const currentTab = { data: tabs[0], url: tabs[0].url }
 
-  console.log(currentTab.data.id);
-  console.log(typeof currentTab.data.id);
-
-  console.log(parseInt(currentTab.data.id));
-  console.log(typeof parseInt(currentTab.data.id));
-
-  const dialogBox = document.getElementById("dialog-box");
+  const dialogBox = document.getElementById("dialog-box")
 
   if (testMediumRegex(currentTab.url)) {
-    dialogBox.innerText = "You are being redirected.";
+    dialogBox.innerText = "You are being redirected."
 
-    const { writerName, restOfURL } = sliceMediumURL(currentTab.url);
+    const { writerName, restOfURL } = sliceMediumURL(currentTab.url)
 
     // chrome.tabs.update();
 
-    console.log(`https://${writerName}.medium.com/${restOfURL}`);
+    //console.log(`https://${writerName}.medium.com/${restOfURL}`);
     chrome.tabs.update(currentTab.data.id, {
       url: `https://${writerName}.medium.com/${restOfURL}`,
-    });
+    })
   } else {
-    dialogBox.innerText = "It isn't a medium article link!";
+    dialogBox.innerText = "It isn't a medium article link!"
   }
-});
+})
 
 function testMediumRegex(url) {
-  const mediumRegex = /.*medium.com\/.+\/./;
-  return mediumRegex.test(url);
+  const mediumRegex = /.*medium.com\/.+\/./
+  return mediumRegex.test(url)
 }
 
 function sliceMediumURL(url) {
-  const urlWithoutHTTP = url.split("//")[1];
-  urlComponents = urlWithoutHTTP.split("/");
-  const writerName = urlComponents[1];
-  const restOfURL = urlComponents.slice(2).join("/");
-  return { writerName, restOfURL };
+  const urlWithoutHTTP = url.split("//")[1]
+  urlComponents = urlWithoutHTTP.split("/")
+  const writerName = urlComponents[1]
+  const restOfURL = urlComponents.slice(2).join("/")
+  return { writerName, restOfURL }
 }
