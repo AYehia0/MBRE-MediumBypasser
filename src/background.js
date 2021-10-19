d34dfcacc540a34d16e090992bcabc18c45ffba1
AYehia0 <ahmedyehia611@gmail.com>
2021-10-19 22:23:50 +0200

    final build, fixed a possible infinite loop as medium doesn't change the link after visiting it

--- src/background.js
+++ src/background.js
@@ -1,55 +1,62 @@
-chrome.tabs.onUpdated.addListener( async () => {
+chrome.tabs.onUpdated.addListener(async (tabid, changeinfo, tab) => {
+  const currentTab = await getCurrentTab()
 
-    const currentTab = await getCurrentTab()
+  // extracting the url
+  const currentUrl = currentTab.url
 
-    // extracting the url
-    const currentUrl = currentTab.url
-
+  // on complete
+  if (tab.url !== undefined && changeinfo.status == "complete") {
     // check if it's a medium url
-    if (isMediumUrl(currentUrl)){
+    if (isMediumUrl(currentUrl)) {
+      // check if url not in localStorage
+      chrome.storage.local.get([currentUrl], (result) => {
+        // console.log(`Current Url : ${currentUrl} is a valid medium url !!!`)
 
-        const newUrl = convertToValidUrl(currentUrl)
+        // console.log(result)
 
-        if (newUrl){
-            // redirect to it
-            chrome.tabs.update({active:true, url: newUrl})
+        // doesn't exist
+        if (!result[currentUrl]) {
+          const newUrl = convertToValidUrl(currentUrl)
 
+          if (newUrl) {
+            // save it to localStorage
+            chrome.storage.local.set({ [currentUrl]: "true" }, () => {
+              // redirect to it
+              chrome.tabs.update({ active: true, url: newUrl })
+            })
+          }
         }
-
-
+      })
     }
-
+  }
 })
-
 
 // is it a medium url
 const isMediumUrl = (url) => {
+  if (url.startsWith("https://medium.com/")) return true
 
-    if (url.startsWith("https://medium.com/"))
-        return true
-
-    return false
+  return false
 }
 
 // converts a url to a new working one
 // https://medium.com/volosoft/whats-new-in-rxjs-7-a11cc564c6c0 -> https://volosoft.medium.com/whats-new-in-rxjs-7-a11cc564c6c0
 const convertToValidUrl = (url) => {
+  try {
+    const splittedUrl = url.split("/")
+    const authorDomain = splittedUrl[3]
+    const restOfUrl = splittedUrl[splittedUrl.length - 1]
 
-    try {
-        const splittedUrl = url.split('/')
-        const authorDomain = splittedUrl[3]
-        const restOfUrl = splittedUrl[splittedUrl.length - 1]
-
-        return `https://${authorDomain}.medium.com/${restOfUrl}`
-    }catch (e) {
-        console.log(e)
-        return null
-    }
+    return `https://${authorDomain}.medium.com/${restOfUrl}`
+  } catch (e) {
+    console.log(e)
+    return null
+  }
 }
 
 // getting the current opended tab
 const getCurrentTab = async () => {
-    let queryOptions = { active: true, currentWindow: true }
-    let [tab] = await chrome.tabs.query(queryOptions)
-    return tab
+  let queryOptions = { active: true, currentWindow: true }
+  let [tab] = await chrome.tabs.query(queryOptions)
+  return tab
 }
+
