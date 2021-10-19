cb1654cfbe7c670c909070b3aa4d9794b85e86ed
AYehia0 <ahmedyehia611@gmail.com>
2021-10-19 11:41:53 +0200

    fixed the zip command not zipping in the correct form

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const fs = require("fs")
 // where the files exists
 const SRC_LOCATION = "src"
 
-// the -q for muting
-const createZIPCommand = `zip -r -q extention_build.zip ${SRC_LOCATION}`
+// -q for muting
+const createZIPCommand = `cd ${SRC_LOCATION} && zip -r -q ../extention_build.zip ${SRC_LOCATION} .`
 
 childProcess.execSync(createZIPCommand)
